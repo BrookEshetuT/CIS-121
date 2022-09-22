06c4a0e9eced175c0cc78f34a7c60d368f9a7e86
BrookEshetuT <vs3541yb@go.minnstate.edu>
2022-09-22 12:19:00 -0500

    add

---
+++ bouncee_qustions.py
@@ -0,0 +1,34 @@
+#def askUserorName():
+ #   name= input("please enter your name:")
+ #   return name
+#name = askUserorName()
+#print(name)
+#name2=askUserorName()
+#print(name2)
+#creeate a function that ask the user for their age then use that value to print out the next 20years 
+#age =int(input("Enter your age:"))
+#for i in range(age,age+21):
+    #print(i)
+
+#string manipulation 
+#print (name[2:7])
+import numbers
+summ=0
+for num in range(6):
+user=int(input("e"))
+summ+=user
+for i in rangr(summm,2001)
+
+
+
+
+
+buy=int(input("enter the amount you bought"))
+a=buy*3
+if a>=50:
+    print("you pay",a-a*.2)
+elif a>=30:
+    print(a*.1)
+
+for i in range(6):
+    print(i[0,1])
